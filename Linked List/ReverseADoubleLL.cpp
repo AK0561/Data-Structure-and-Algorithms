d04b6eb543e908d4b5b308284db3f46189a2a6fe
Arnav Aggarwal <97022114+AK0561@users.noreply.github.com>
2024-01-22 19:52:23 +0530

    Reverse a Doubly Linked List

---
+++ Linked List/ReverseADoubleLL.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* back;
+    Node(int data1, Node* next1, Node* back1) : data(data1), next(next1), back(back1) {}
+    Node(int data1) : data(data1), next(nullptr), back(nullptr) {}
+};
+
+Node* Array2DLL(vector<int> arr){
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+Node* ReverseLL(Node* head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    Node* last=NULL;
+    Node* current=head;
+    while(current!=NULL){
+        last=current->back;
+        current->back=current->next;
+        current->next=last;
+        current=current->back;
+    }
+    return last->back;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    Node* head=Array2DLL(arr);
+    head=ReverseLL(head);
+    print(head);
+    return 0;
+}
