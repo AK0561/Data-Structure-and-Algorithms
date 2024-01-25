e29361554767d14b40f1f6c0e32af3f02c51931c
Arnav Aggarwal <97022114+AK0561@users.noreply.github.com>
2024-01-25 13:08:21 +0530

    Palindrome in a Linked List

---
+++ Linked List/PalindromeLL.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node* back;
+    Node(int data1, Node* next1, Node* back1) : data(data1), next(next1), back(back1) {}
+    Node(int data1) : data(data1), next(nullptr), back(nullptr) {}
+};
+
+Node* Array2DLL(vector<int> arr){
+    Node* head=new Node(arr[0]);
+    Node* prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp=new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
+
+Node* ReverseLL(Node* head){
+    Node* temp = head;
+    Node* prevNode = NULL;
+    while( temp !=NULL ){
+        Node* front = temp->next;
+        temp->next = prevNode;
+        prevNode = temp;
+        temp = front;
+    }
+    return prevNode;
+}
+
+bool isPalindrome(Node* head){
+    if( head == NULL || head->next == NULL ){
+        return true;
+    }
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* newHead = ReverseLL(slow->next);
+    Node* first = head;  
+    Node* second = newHead; 
+    while (second != NULL) {
+        if (first->data != second->data) {
+            ReverseLL(newHead);  
+            return false;
+        }
+        first = first->next; 
+        second = second->next;  
+    }
+    ReverseLL(newHead);  
+    return true;  
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    Node* head=Array2DLL(arr);
+    cout<<isPalindrome(head);
+    return 0;
+}
